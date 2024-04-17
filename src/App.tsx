7a1015d33f9544699d4d53a8362fc46b26f6bdd8
Songhee Heo <songheeheo@Songheeui-MacBookPro.local>
2024-04-17 16:29:02 +0900

    Header 컴포넌트

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from '@emotion/styled';
+import { Header } from 'components/Header';
+
+const Container = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #eeeeee;
+  overflow: scroll;
+`;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header />
+    </Container>
   );
 }
 
