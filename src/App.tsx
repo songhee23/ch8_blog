8198ba68356f0fc311a2870b43be280b4cf560c6
Songhee Heo <songheeheo@Songheeui-MacBookPro.local>
2024-04-17 16:41:32 +0900

    BlogPost 컴포넌트화

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import styled from '@emotion/styled';
 import { Header } from 'components/Header';
+import { BlogPost } from 'components/BlogPost'
 
 const Container = styled.div`
   height: 100vh;
@@ -17,6 +18,9 @@ function App() {
   return (
     <Container>
       <Header />
+      <BlogPost 
+        title="sunt aut facere repellat provident occaecati excepturi optio reprehenderit" 
+        body="quia et suscipit nsuscipit recusandae consequuntur expedita et cum nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"/>
     </Container>
   );
 }
